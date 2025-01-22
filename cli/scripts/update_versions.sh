4ee2e81c03cd809b1f83dbec7795f410e82a10c6
Steven Isbell <steven.isbell18@gmail.com>
2025-01-22 15:23:40 -0700

    fix quotes

--- cli/scripts/update_versions.sh
+++ cli/scripts/update_versions.sh
@@ -9,6 +9,6 @@ fi
 
 echo "Updating versions to $VERSION for $BRANCH_NAME"
 
-jq ".version=\"'"$VERSION"'\"" package.json > package.json.tmp && mv package.json.tmp package.json
+jq ".version=\"${VERSION}\"" package.json > package.json.tmp && mv package.json.tmp package.json
 
-jq "if has(\"dependencies\") then .dependencies |= with_entries(select(.key | startswith(\"@angel-studios/\")).value=\"'"$VERSION"'\") else . end" package.json > package.json.tmp && mv package.json.tmp package.json
+jq 'if has("dependencies") then .dependencies |= with_entries(select(.key | startswith("@angel-studios/")).value="'${VERSION}'") else . end' package.json > package.json.tmp && mv package.json.tmp package.json

7d50866847d0a81929c76c488889eee06198bb1b
Steven Isbell <steven.isbell18@gmail.com>
2025-01-22 15:00:48 -0700

    update script

--- cli/scripts/update_versions.sh
+++ cli/scripts/update_versions.sh
@@ -9,10 +9,6 @@ fi
 
 echo "Updating versions to $VERSION for $BRANCH_NAME"
 
-yarn spaces -p exec -- bash -c '
-  jq ".version=\"'"$VERSION"'\"" package.json > package.json.tmp && mv package.json.tmp package.json
-'
+jq ".version=\"'"$VERSION"'\"" package.json > package.json.tmp && mv package.json.tmp package.json
 
-yarn spaces -p exec -- bash -c '
-  jq "if has(\"dependencies\") then .dependencies |= with_entries(select(.key | startswith(\"@angel-studios/\")).value=\"'"$VERSION"'\") else . end" package.json > package.json.tmp && mv package.json.tmp package.json
-'
+jq "if has(\"dependencies\") then .dependencies |= with_entries(select(.key | startswith(\"@angel-studios/\")).value=\"'"$VERSION"'\") else . end" package.json > package.json.tmp && mv package.json.tmp package.json

51a70e89b016500693b8735204ad7f0ef4b81e6a
swathid26 <swathi.devaruppala@ftr.com>
2025-02-18 17:38:42 -0500

    created terraform template and pipeline for nonprod

---
+++ terraform/version.tf
@@ -0,0 +1,18 @@
+
+terraform {
+  cloud {
+    organization = "frontier-devops"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.1.0"
+    }
+  }
+  required_version = "~> 1.0"
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration  = true
+}

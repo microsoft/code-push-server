cbee9ad8a311bb4c8a04fbbc44f010a0ca072fb0
swathid26 <swathi.devaruppala@ftr.com>
2025-02-19 16:12:16 -0500

    added redis private endpoint

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,10 +37,13 @@ resource "azurerm_app_service" "root" {
     CORS_ORIGIN = var.server_url
     SERVER_URL = var.server_url
   }
-#    site_config {
-#      linux_fx_version = "NODE|18.x"
-#      always_on = "true"
-#    }
+  site_config {
+   ip_restriction {
+      name                      = "Allow access from vnet"
+      virtual_network_subnet_id = azurerm_subnet.subnet.id
+      priority                  = "200"
+    }
+  } 
   identity {
     type = "SystemAssigned"
   }
@@ -67,12 +70,13 @@ resource "azurerm_virtual_network" "virtual_network" {
   location            = azurerm_resource_group.root.location
   resource_group_name = azurerm_resource_group.root.name
   address_space       = ["10.0.0.0/16"]
+}
 
-  subnet {
-    name             = "subnet1"
-    address_prefixes = ["10.0.1.0/24"]
-    security_group   = azurerm_network_security_group.network_security.id
-  }
+resource "azurerm_subnet" "subnet" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.root.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_redis_cache" "redis" {
@@ -91,6 +95,42 @@ resource "azurerm_redis_cache" "redis" {
   }
 }
 
+resource "azurerm_app_service_virtual_network_swift_connection" "virtual_network_connection" {
+  app_service_id = azurerm_app_service.root.id
+  subnet_id      = azurerm_subnet.subnet.id
+}
+
+resource "azurerm_private_dns_zone" "dns" {
+  name                = "codepush.privatelink.redis.cache.windows.net"
+  resource_group_name = azurerm_resource_group.root.name
+}
+
+resource "azurerm_private_endpoint" "redis_private_endpoint" {
+  name                = "codepush-redis-private-endpoint-${var.environment}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.root.name
+  subnet_id           = azurerm_subnet.subnet.id
+
+   private_dns_zone_group {
+    name                 = "codepushprivatednsrediszonegroup"
+    private_dns_zone_ids = [azurerm_private_dns_zone.dns.id]
+  }
+
+  private_service_connection {
+    name                           = "codepushserviceconnection"
+    private_connection_resource_id = azurerm_redis_cache.redis.id
+    is_manual_connection           = false
+    subresource_names              = ["redisCache"]
+  }
+}
+
+
+resource "azurerm_private_dns_zone_virtual_network_link" "virtual_network_link" {
+  name                  = "codepush-virtual-link-${var.environment}"
+  private_dns_zone_name = azurerm_private_dns_zone.dns.name
+  virtual_network_id    = azurerm_virtual_network.virtual_network.id
+  resource_group_name   = azurerm_resource_group.root.name
+}
 // Auto-scaling policy
 
  
